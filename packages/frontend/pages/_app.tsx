3cb0523aaa88b6eb37b56024328a4390a7f398b6
boutchaz <zakaria.boutchamir@4d.com>
2022-04-17 15:04:19 +0000

    chore : initialize nextjs and tailwind

---
+++ packages/frontend/pages/_app.tsx
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
+   <Component {...pageProps} />
+  </div>
+  )
+}
+
+export default MyApp
---
+++ packages/frontend/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <p className="text-red-600">The quick brown fox...</p>
+
+  )
+}
+
+export default Home
